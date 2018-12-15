8e99fca6244912ffe2aa7e093de08505d1dde753
CSUaltitue <703362086@qq.com>
2018-12-15 22:36:55 +0800

    add RCNN learning resources

---
+++ EasyPRLearning/RCNN/RCNN_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 15 22:03:17 2018
+
+@author: zhouwei
+"""
+
+import tflearn 
+from tflearn.layers import core
+from tflearn.layers import conv
+from tflearn.layers import input_data
+from tflearn.layers import fully_connected
